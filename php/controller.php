a84f3737dfe464431cbef4cbcc4dfc3b604d4133
Unknown <leo.tillieu@isen.yncrea.fr>
2018-11-19 18:28:23 +0100

    oops

--- php/controller.php
+++ php/controller.php
@@ -15,6 +15,7 @@ function redirect($link = NULL){
 function connectDb(){
     try {
         $opts = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+        $bdd = new PDO("mysql:host=localhost;dbname=pad;charset=utf8","root", "isencir", $opts);
         return $bdd;
     } catch (Exception $e) {
         exit('Impossible to connect to database.');
